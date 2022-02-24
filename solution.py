df6d2b0faf05c4d7a2b1fee995f7115384203b92
Giorgio Attorresi <attorresi.giorgio@gmail.com>
2022-02-24 22:44:04 +0100

    Solution

---
+++ solution.py
@@ -0,0 +1,76 @@
+from operator import attrgetter
+
+
+f = open('a_an_example.in.txt',"rt")
+c, p = f.readline().strip().split()
+class Contirbutor:
+    def __init__(self,name,skills,available):
+        self.name = name
+        self.skills = skills
+        self.available = available
+
+class Project:
+    def __init__(self,name, time, score, ddl, nb_contrib, skills):
+        self.name = name
+        self.time = time
+        self.score = score
+        self.ddl = ddl
+        self.nb_contrib = nb_contrib
+        self.skills = skills
+
+class Skill:
+    def __init__(self, name, level, contributor):
+        self.name = name
+        self.level = level
+        self.contributor = contributor
+
+contributors=[]
+for i in range(int(c)):
+    name, nb_skill=f.readline().strip().split()
+    skills=[]
+    for j in range(int(nb_skill)):
+        skill, level = f.readline().strip().split()
+
+        skills.append(Skill(skill,int(level),None))
+    contributors.append(Contirbutor(name,skills,True))
+
+projects=[]
+for i in range(int(p)):
+    name, time, score, ddl, nb_contrib = f.readline().strip().split()
+    skills=[]
+    for j in range(int(nb_contrib)):
+        skill, level = f.readline().strip().split()
+        skills.append(Skill(skill,int(level),None))
+    projects.append(Project(name, int(time), int(score), int(ddl), int(nb_contrib),skills))
+
+
+projects.sort(key=attrgetter('ddl')) 
+days=0
+for p in projects:
+    days += p.time
+
+cpt = 0
+
+affected=[]
+
+for p in projects:
+    for c in contributors:
+        for p_skill in p.skills:
+            for c_skill in c.skills:
+                if( p_skill.name == c_skill.name and c_skill.level >= p_skill.level):
+                    affected.append(c)
+
+completed=[]
+
+for p in projects:
+    if(p.nb_contrib == len(affected)):
+        completed.append(p)
+
+f1 = open("a.out.txt", "w")
+
+f1.write(str(len(projects)-8)+"\n")
+for p in projects:
+    f1.write(p.name+"\n")
+    for c in affected:
+        f1.write(c.name+" ")
+    f1.write("\n")
